f0123e3287167a4d2f358921306407d9714952f8
leaojn <arealeaoneto@gmail.com>
2017-08-23 21:44:50 -0300

    alteracoes models

--- board/urls.py
+++ board/urls.py
@@ -5,10 +5,3 @@ from . import views
 router = DefaultRouter()
 
 router.register(r'professores', views.ProfessorViewSet)
-from django.conf.urls import url,include
-from .views import pdf
-
-urlpatterns = [
-    url('^pdf/$', views.pdf, name='meus_eventos'),
-
-]
