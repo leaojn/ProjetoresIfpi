80f416b9338180586f0ee690772893a3198a6e71
leaojn <arealeaoneto@gmail.com>
2017-08-23 21:43:33 -0300

    alteracoes models

--- board/views.py
+++ board/views.py
@@ -1,9 +1,7 @@
 from rest_framework import viewsets, authentication, permissions
 from .models import Professor
 from .serializers import ProfessorSerializer
-from reportlab.pdfgen import canvas
-from django.http import HttpResponse
-from django.contrib.auth import get_user_model
+
 
 class ProfessorViewSet(viewsets.ModelViewSet):
 
@@ -12,10 +10,3 @@ class ProfessorViewSet(viewsets.ModelViewSet):
     search_fields = ('name')
     ordering_fields = ('name')
 
-def pdf(request):
-    template_name = 'pdf.html'
-    pdf = canvas.Canvas("teste.pdf")
-    pdf.drawString(100,100,"Hello Word")
-    pdf.showPage()
-    pdf.save()
-    return "teste.pdf"
